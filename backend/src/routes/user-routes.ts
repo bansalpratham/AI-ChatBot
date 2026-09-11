c94aaa6040d4657226ab0a0aeb6e713bdd0ec359
bansalpratham <prathambansal200@gmail.com>
2026-09-11 16:36:35 +0530

    Implement user signup functionality and update user routes

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { getAllUsers } from '../controllers/user-controllers.js';
+import { getAllUsers, userSignup } from '../controllers/user-controllers.js';
 
 const userRoutes = Router();
 
 userRoutes.get("/",getAllUsers)
+userRoutes.get("/signup",userSignup)
 
 export default userRoutes;
