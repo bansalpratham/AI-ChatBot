9a37d9cf6ddcb85a0cbe29a431ffd1cffbca9ce3
bansalpratham <prathambansal200@gmail.com>
2026-09-12 11:46:22 +0530

    Implement user login functionality and update user routes with validation

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { getAllUsers, userSignup } from '../controllers/user-controllers.js';
-import { signupValidator, validate } from '../utils/validators.js';
+import { loginValidator, signupValidator, validate } from '../utils/validators.js';
 
 const userRoutes = Router();
 
 userRoutes.get("/",getAllUsers)
 userRoutes.get("/signup",validate(signupValidator),userSignup)
+userRoutes.get("/login",validate(loginValidator),userSignup)
 
 export default userRoutes;
