20e705c539f8f07cb1d787add584c4320dff13bc
bansalpratham <prathambansal200@gmail.com>
2026-09-12 12:13:15 +0530

    Add authentication token management and cookie handling for user login and signup

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import User from "../models/User.js"
 import {hash,compare} from 'bcrypt'
+import { createToken } from "../utils/token-manager.js"
+import { COOKIE_NAME } from "../utils/constants.js"
 
 export const getAllUsers = async (req:Request,res:Response,next:NextFunction)=>{
     try {
@@ -30,6 +32,24 @@ export const userSignup = async (req:Request,res:Response,next:NextFunction)=>{
 
         await user.save();
 
+         res.clearCookie(COOKIE_NAME , {
+              domain: "localhost",
+            httpOnly: true,
+            signed: true,
+            path:"/",
+        });
+
+        const token = createToken(user._id.toString(),user.email,"7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME,token,{
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        });
+
          return res.status(200).json({message:"OK",id:user._id.toString()})
 
     } catch (error) {
@@ -53,6 +73,24 @@ export const userLogin = async (req:Request,res:Response,next:NextFunction)=>{
         {
             return res.status(403).send("Incorrect Password");       
         }
+
+        res.clearCookie(COOKIE_NAME , {
+              domain: "localhost",
+            httpOnly: true,
+            signed: true,
+            path:"/",
+        });
+
+        const token = createToken(user._id.toString(),user.email,"7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME,token,{
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true,
+        });
 
          return res.status(201).json({message:"OK",id:user._id.toString()})
 
