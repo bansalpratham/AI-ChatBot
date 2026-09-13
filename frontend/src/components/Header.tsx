5124bb62b22639f97f8cebaf0e38b85db5c6fc57
bansalpratham <prathambansal200@gmail.com>
2026-09-13 14:51:00 +0530

    feat: add routing and header components with logo to the application

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { AppBar, Toolbar } from '@mui/material'
+import Logo from './shared/Logo'
 
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <AppBar sx={{bgcolor: "transparent",position: "static",boxShadow:"none"}} >
+    <Toolbar sx={{display: "flex"}} >
+    <Logo/>
+    </Toolbar>
+  </AppBar>
 }
 
 export default Header
