c5fd693cf60db2dd7e36ebd356a034172b6d2593
bansalpratham <prathambansal200@gmail.com>
2026-09-18 19:25:06 +0530

    refactor: enhance CustomizedInput component with improved formatting and styling

--- frontend/src/components/shared/CustomizedInput.tsx
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -1,13 +1,32 @@
 import { TextField } from '@mui/material';
-import React from 'react'
+import React from 'react';
+
 type Props = {
-    name: string;
-    type: string;
-    label: string;
+  name: string;
+  type: string;
+  label: string;
 };
 
 function CustomizedInput(props: Props) {
-  return <TextField name={props.name} label={props.label} type={props.type} />
+  return (
+    <TextField
+      margin="normal"
+      name={props.name}
+      label={props.label}
+      type={props.type}
+      sx={{
+        '& .MuiInputBase-root': {
+          width: '400px',
+          borderRadius: '10px',
+          fontSize: '20px',
+          color: 'white',
+        },
+        '& .MuiInputLabel-root': {
+          color: 'white',
+        },
+      }}
+    />
+  );
 }
 
 export default CustomizedInput
